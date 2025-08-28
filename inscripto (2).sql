5aae2a57106c5179cfddc295b873210abb5d33ee
Mateo <mateobaute10@gmail.com>
2025-08-28 00:11:56 -0300

    Add files via upload

---
+++ inscripto (2).sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 28-08-2025 a las 05:11:44
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `inscriptos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `inscripto`
+--
+
+CREATE TABLE `inscripto` (
+  `id` int(11) NOT NULL,
+  `nombre_completo` varchar(40) NOT NULL,
+  `edad` varchar(3) NOT NULL,
+  `nmro_teléfono` varchar(15) NOT NULL,
+  `cédula` varchar(8) NOT NULL,
+  `categoría` varchar(11) NOT NULL,
+  `género` varchar(11) NOT NULL,
+  `talla` varchar(1) NOT NULL,
+  `distancia` varchar(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `inscripto`
+--
+ALTER TABLE `inscripto`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `inscripto`
+--
+ALTER TABLE `inscripto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
